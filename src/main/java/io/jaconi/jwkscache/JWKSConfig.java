a9b958db58e397075765f0fd74ddb0c580c632bc
Julian Nodorp <jnodorp@jaconi.io>
2023-09-21 16:38:07 +0200

    feat: warmup cache on startup

--- src/main/java/io/jaconi/jwkscache/JWKSConfig.java
+++ src/main/java/io/jaconi/jwkscache/JWKSConfig.java
@@ -6,6 +6,9 @@ import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.nimbusds.jose.KeySourceException;
+import com.nimbusds.jose.jwk.JWKMatcher;
+import com.nimbusds.jose.jwk.JWKSelector;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.jwk.source.JWKSourceBuilder;
 import com.nimbusds.jose.proc.SecurityContext;
@@ -26,11 +29,20 @@ public class JWKSConfig {
 
 	private JWKSource<SecurityContext> jwkSource(URL url) {
 		log.info("caching JWKS for endpoint {}", url);
-		return JWKSourceBuilder.create(url)
+		var source = JWKSourceBuilder.create(url)
 				.healthReporting(healthReportListenerFactory.create(url))
 				.retrying(true)
 				// Handle outages of the JWKS source for up to 50 Minutes (default).
 				.outageTolerant(true)
 				.build();
+
+		// Warmup cache on startup.
+		try {
+			source.get(new JWKSelector(new JWKMatcher.Builder().build()), null);
+		} catch (KeySourceException e) {
+			log.warn("exception during cache warmup for {}", url, e);
+		}
+
+		return source;
 	}
 }
