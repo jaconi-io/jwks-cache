4fe9fc54190f18ee23cac620fd45efda81c6e37d
Julian Nodorp <jnodorp@jaconi.io>
2025-10-24 10:14:35 +0200

    feat: make caching and outage tolerance configurable

--- src/main/java/io/jaconi/jwkscache/JWKSConfig.java
+++ src/main/java/io/jaconi/jwkscache/JWKSConfig.java
@@ -1,11 +1,5 @@
 package io.jaconi.jwkscache;
 
-import java.net.URL;
-import java.util.List;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
 import com.nimbusds.jose.KeySourceException;
 import com.nimbusds.jose.jwk.JWKMatcher;
 import com.nimbusds.jose.jwk.JWKSelector;
@@ -13,9 +7,13 @@ import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.jwk.source.JWKSourceBuilder;
 import com.nimbusds.jose.proc.SecurityContext;
 import com.nimbusds.jose.util.ResourceRetriever;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.URL;
+import java.util.List;
 
 @Slf4j
 @Configuration
@@ -23,20 +21,37 @@ import lombok.extern.slf4j.Slf4j;
 public class JWKSConfig {
 	private final HealthReportListenerFactory healthReportListenerFactory;
 	private final ResourceRetriever resourceRetriever;
+	private final JWKSProperties jwksProperties;
 
 	@Bean
-	public List<JWKSource<SecurityContext>> jwkSetSource(JWKSProperties jwksProperties) {
+	public List<JWKSource<SecurityContext>> jwkSetSource() {
 		return jwksProperties.urls().stream().map(this::jwkSource).toList();
 	}
 
 	private JWKSource<SecurityContext> jwkSource(URL url) {
 		log.info("caching JWKS for endpoint {}", url);
-		var source = JWKSourceBuilder.create(url, resourceRetriever)
+		var builder = JWKSourceBuilder.create(url, resourceRetriever)
 				.healthReporting(healthReportListenerFactory.create(url))
-				.retrying(true)
-				// Handle outages of the JWKS source for up to 50 Minutes (default).
-				.outageTolerant(true)
-				.build();
+				.retrying(true);
+
+		if (jwksProperties.caching().enabled()) {
+			builder.cache(jwksProperties.caching().timeToLive().toMillis(), JWKSourceBuilder.DEFAULT_CACHE_REFRESH_TIMEOUT);
+		} else {
+			log.warn("caching should not be disabled in production");
+			builder.cache(false);
+			log.warn("disabling caching disabled rate limiting");
+			builder.rateLimited(false);
+			log.warn("disabling caching disabled refresh-ahead caching");
+			builder.refreshAheadCache(false);
+		}
+
+		if (jwksProperties.outageTolerance().enabled()) {
+			builder.outageTolerant(jwksProperties.outageTolerance().timeToLive().toMillis());
+		} else {
+			builder.outageTolerant(false);
+		}
+
+		var source = builder.build();
 
 		// Warmup cache on startup.
 		try {
---
+++ src/test/java/io/jaconi/jwkscache/CachingTest.java
@@ -0,0 +1,70 @@
+package io.jaconi.jwkscache;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockserver.integration.ClientAndServer;
+import org.mockserver.junit.jupiter.MockServerExtension;
+import org.mockserver.junit.jupiter.MockServerSettings;
+import org.mockserver.model.HttpError;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockserver.model.HttpRequest.request;
+import static org.mockserver.model.HttpResponse.response;
+
+@ExtendWith(MockServerExtension.class)
+@MockServerSettings(ports = 8082)
+@SpringBootTest(properties = {
+		"jaconi.jwks.caching.enabled = true",
+		"jaconi.jwks.outage-tolerance.enabled = false",
+		"jaconi.jwks.urls = http://localhost:8082"
+})
+class CachingTest {
+	private final ClientAndServer client;
+
+	@Autowired
+	private WellKnownController wellKnownController;
+
+	public CachingTest(ClientAndServer client) {
+		this.client = client;
+	}
+
+	@BeforeEach
+	public void setUp() {
+		client.reset();
+	}
+
+	@Test
+	void testCaching() {
+		client.when(request().withMethod("GET")).respond(response().withBody("""
+				{
+				  "keys": [
+				    {
+				      "e": "AQAB",
+				      "use": "sig",
+				      "kid": "fb9f9371d5755f3e383a40ab3a172cd8baca517f",
+				      "n": "to2hcsFNHKquhCdUzXWdP8yxnGqxFWJlRT7sntBgp47HwxB9HFc-U_AB1JT8xe1hwDpWTheckoOfpLgo7_ROEsKpVJ_OXnotL_dgNwbprr-T_EFJV7qOEdHL0KmrnN-kFNLUUSqSChPYVh1aEjlPfXg92Yieaaz2AMMtiageZrKoYnrGC0z4yPNYFj21hO1x6mvGIjmpo6_fe91o-buZNzzkmYlGsFxdvUxYAvgk-5-7D10UTTLGh8bUv_BQT3aRFiVRS5d07dyCJ4wowzxYlPSM6lnfUlvHTWyPL4JysMGeu-tbPA-5QvwCdSGpfWFQbgMq9NznBtWb99r1UStpBQ",
+				      "kty": "RSA",
+				      "alg": "RS256"
+				    }
+				  ]
+				}
+				"""));
+
+		var response = wellKnownController.jwksJSON();
+		assertThat(response).isNotNull();
+		assertThat(response).containsKey("keys");
+		assertThat(response.get("keys")).hasSize(1);
+
+		// Simulate an outage.
+		client.when(request().withMethod("GET")).error(HttpError.error().withDropConnection(true));
+
+		// Expect a non-empty response despite outage tolerance being disabled.
+		response = wellKnownController.jwksJSON();
+		assertThat(response).isNotNull();
+		assertThat(response).containsKey("keys");
+		assertThat(response.get("keys")).hasSize(1);
+	}
+}
