3f5bd63813834397bf93ce0e0a6b2ac026995147
Julian Nodorp <jnodorp@jaconi.io>
2026-06-24 08:17:11 +0200

    fix: reachability metadata

--- src/main/java/io/jaconi/jwkscache/Application.java
+++ src/main/java/io/jaconi/jwkscache/Application.java
@@ -3,8 +3,10 @@ package io.jaconi.jwkscache;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ImportRuntimeHints;
 
 @SpringBootApplication
+@ImportRuntimeHints(RuntimeHints.class)
 @EnableConfigurationProperties(JWKSProperties.class)
 public class Application {
 	public static void main(String[] args) {
